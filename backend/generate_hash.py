9740a38774888c6312dcf7375ae28210e0a957de
Pallab Bormon <pallabbarman02@gmail.com>
2026-08-14 16:17:29 +0600

    Remove sensitive files from tracking

--- backend/generate_hash.py
+++
@@ -1,7 +0,0 @@
-from passlib.context import CryptContext
-
-ctx = CryptContext(schemes=["bcrypt"])
-
-password = "*#pallab8640#*"
-
-print(ctx.hash(password))
--- backend/test_auth.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-ctx = CryptContext(schemes=["bcrypt"])
-
-password = "*#P@ll@b@2224#*"
-hash_value = "$2b$12$sQOu423rLjHMlT8UC9zoMulvf/PsVVGvO5izyvSftEQeuWMJ3PwRK"
-
-print(ctx.verify(password, hash_value))
